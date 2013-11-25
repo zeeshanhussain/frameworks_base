925286c9a500fb25eb79c36a47fb8566c68fe935
xplodwild <xplod@ouverta.fr>
2013-11-25 22:54:39 +0100

    [1/2] SystemUI: Add time-context headers to the notification header
    This commit adds image headers to the notification panel header,
    based on the date/time.
    
    There is a default provider that provides sky pictures
    as well as special events like Christmas and new
    year's eve.
    
    More will be coming, with eventually a way to use an external
    (as in, Internet) provider for those pictures.
    
    Change-Id: Ie3321e9a3e0637d6ba8094dd46aee36c41354713
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarHeaderView.java
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -134,6 +134,7 @@ import com.android.systemui.cm.UserContentObserver;
 import com.android.systemui.doze.DozeHost;
 import com.android.systemui.doze.DozeLog;
 import com.android.systemui.keyguard.KeyguardViewMediator;
+import com.android.systemui.omni.StatusBarHeaderMachine;
 import com.android.systemui.qs.QSDragPanel;
 import com.android.systemui.qs.QSPanel;
 import com.android.systemui.recents.ScreenPinningRequest;
@@ -586,6 +587,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
     private boolean mAutohideSuspended;
     private int mStatusBarMode;
     private int mNavigationBarMode;
+    private StatusBarHeaderMachine mStatusBarHeaderMachine;
 
     private ViewMediatorCallback mKeyguardViewMediatorCallback;
     private ScrimController mScrimController;
@@ -1235,6 +1237,9 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         // Private API call to make the shadows look better for Recents
         ThreadedRenderer.overrideProperty("ambientRatio", String.valueOf(1.5f));
 
+        mStatusBarHeaderMachine = new StatusBarHeaderMachine(mContext);
+        mStatusBarHeaderMachine.addObserver(mHeader);
+        mStatusBarHeaderMachine.updateEnablement();
         return mStatusBarView;
     }
 
